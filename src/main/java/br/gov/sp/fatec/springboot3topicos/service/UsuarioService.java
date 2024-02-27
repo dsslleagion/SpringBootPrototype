448cd46497bb0d82de8cdea569920301aa85d812
Dionisio Samuel Dos Santos Leão <dionisio.leao@fatec.sp.gov.br>
2024-02-27 13:36:17 +0000

    27_02

---
+++ src/main/java/br/gov/sp/fatec/springboot3topicos/service/UsuarioService.java
@@ -0,0 +1,45 @@
+package br.gov.sp.fatec.springboot3topicos.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.gov.sp.fatec.springboot3topicos.entity.Usuario;
+import br.gov.sp.fatec.springboot3topicos.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService{
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    public List <Usuario> buscarTodosUsuarios() {
+        List<Usuario> usuarios = usuarioRepo.findAll();
+       return usuarios;
+    }
+
+    public Usuario cadastraUsuario(Usuario usuario){
+        if(usuario == null || 
+        usuario.getNome() == null ||
+        usuario.getNome().isBlank() ||
+        usuario.getSenha() == null ||
+        usuario.getSenha().isBlank() ){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "erro");
+        }
+        return usuarioRepo.save(usuario);
+    }
+
+    public Usuario buscarUsuarioPorId(Long id){
+        Optional<Usuario> usuarioOP = usuarioRepo.findById(id);
+        if(usuarioOP.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "erro");
+        }
+        return usuarioOP.get();
+    }
+
+
+}
