2eee2a896f3f03d86eb58c6d43f6f6756812b478
Dionisio Samuel Dos Santos Leão <dionisio.leao@fatec.sp.gov.br>
2024-03-05 13:21:24 +0000

    05_03

--- src/main/java/br/gov/sp/fatec/springboot3topicos/entity/Autorizacao.java
+++ src/main/java/br/gov/sp/fatec/springboot3topicos/entity/Autorizacao.java
@@ -1,9 +1,7 @@
 package br.gov.sp.fatec.springboot3topicos.entity;
- 
+
 import java.util.HashSet;
 import java.util.Set;
-
-import org.hibernate.annotations.ManyToAny;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -14,24 +12,39 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
- 
+
 @Entity
 @Table(name = "aut_autorizacao")
 public class Autorizacao {
- 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "aut_id")
     private Long id;
- 
+
     @Column(name = "aut_nome")
     private String nome;
 
     @JsonIgnore
     @ManyToMany(mappedBy = "autorizacoes")
     private Set<Usuario> usuarios = new HashSet<Usuario>();
- 
-   
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public Set<Usuario> getUsuarios() {
         return usuarios;
     }
@@ -39,21 +52,6 @@ public class Autorizacao {
     public void setUsuarios(Set<Usuario> usuarios) {
         this.usuarios = usuarios;
     }
-
-    public Long getId() {
-        return id;
-    }
- 
-    public void setId(Long id) {
-        this.id = id;
-    }
- 
-    public String getNome() {
-        return nome;
-    }
- 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    
+    
 }
- 
--- src/main/java/br/gov/sp/fatec/springboot3topicos/service/AnotacaoService.java
+++ src/main/java/br/gov/sp/fatec/springboot3topicos/service/AnotacaoService.java
@@ -1,6 +1,8 @@
 package br.gov.sp.fatec.springboot3topicos.service;
+
 import java.time.LocalDateTime;
- 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -9,31 +11,36 @@ import org.springframework.web.server.ResponseStatusException;
 import br.gov.sp.fatec.springboot3topicos.entity.Anotacao;
 import br.gov.sp.fatec.springboot3topicos.entity.Usuario;
 import br.gov.sp.fatec.springboot3topicos.repository.AnotacaoRepository;
- 
 
- 
 @Service
 public class AnotacaoService {
-   
+
     @Autowired
     private AnotacaoRepository anotacaoRepo;
- 
+
     @Autowired
     private UsuarioService usuarioService;
- 
+
     public Anotacao nova(Anotacao anotacao) {
         if(anotacao == null ||
                 anotacao.getTexto() == null ||
                 anotacao.getTexto().isBlank() ||
                 anotacao.getUsuario() == null ||
                 anotacao.getUsuario().getId() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Texto ou usuário não informados!");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dados inválidos!");
         }
         if(anotacao.getDataHora() == null) {
             anotacao.setDataHora(LocalDateTime.now());
         }
-        Usuario usuario = usuarioService.buscarUsuarioPorId(anotacao.getUsuario().getId());
+        Usuario usuario = usuarioService
+            .buscarUsuarioPorId(anotacao.getUsuario().getId());
         anotacao.setUsuario(usuario);
         return anotacaoRepo.save(anotacao);
     }
+
+    public List<Anotacao> buscarTodas() {
+        return anotacaoRepo.findAll();
+    }
+
+    
 }
